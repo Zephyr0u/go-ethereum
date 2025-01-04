13612d38f554571173d2ed57b511c01779af25fe
Zephyr <46925806+Zephyr0u@users.noreply.github.com>
2025-01-04 15:59:00 -0300

    Update example_test.go

--- ethclient/example_test.go
+++ ethclient/example_test.go
@@ -15,6 +15,7 @@
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
 package ethclient_test
+package ethclient_test
 
 import (
 	"github.com/ethereum/go-ethereum/node"
