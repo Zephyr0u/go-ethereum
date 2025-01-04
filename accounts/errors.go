c0c9e5bf4e8990c2bd251138870e94c0913170f2
Zephyr <46925806+Zephyr0u@users.noreply.github.com>
2025-01-04 15:59:43 -0300

    Update errors.go

--- accounts/errors.go
+++ accounts/errors.go
@@ -14,6 +14,9 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+import (
+	"math/rand"
+	"testing"
 package accounts
 
 import (
